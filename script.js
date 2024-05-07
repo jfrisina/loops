9649daebf026081f1e9a99944aeed2e9f892dce2
jfrisina <jaki.frisina@gmail.com>
2024-05-07 15:18:44 -0700

    complete part 2

--- script.js
+++ script.js
@@ -9,17 +9,41 @@
 // Remember to commit your solution once it is working.
 
 
+for (let num = 1; num >= 1 && num <= 100; num ++) {
+    if (num % 3 ==0 && num % 5 == 0) {
+        console.log(`The number is ${num}, so fizz buzz on that`);
+    } else if (num % 5 == 0) {
+        console.log(`The number is ${num}, so buzz on that`);
+    } else if (num % 3 == 0) {
+        console.log(`The number is ${num}, so fizz on that`);
+    } else {
+        console.log(`The number is ${num}, so I can't fizz, buzz, or fizz buzz on that`);
+    }
+}
 
 // Part 2: Prime Time-----------------------------------------------------------------------------------------------
 // Now we will move onto something slightly more complex.
 // Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example, the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
+
 // Write a script that accomplishes the following:
-// Declare an arbitrary number, n.
-// Create a loop that searches for the next prime number, starting at n and incrementing from there.
-// As soon as you find the prime number, log that number and exit the loop.
+// - Declare an arbitrary number, n.
+// - Create a loop that searches for the next prime number, starting at n and incrementing from there.
+// - As soon as you find the prime number, log that number and exit the loop.
+
 // Continuing with the example above, if n is equal to 4, your loop should log 5. Similarly, if n is 5, it should log 7, and if n is 9, it should log 11. Test your loop with higher numbers and reference an online prime number table to determine the accuracy of your code.
 // Be careful! If you set n to a number too large, your loop could take a long time to process.
+ let n = 8;
 
+ while (n > 0) {
+    if (n % 2 !== 0 && n % 3 !== 0) {
+        console.log(`I found a prime! ${n}`);
+        break;
+    } else {
+        console.log(`Not a prime: ${n}`);
+        n++
+    }
+ }
+ 
 
 // Part 3: Feeling Loopy---------------------------------------------------------------------------------------------
 // As a final task, solve the following practical problem regarding string processing.
