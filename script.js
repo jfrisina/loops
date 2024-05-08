c90284fa6af99801db87851093eeafbfdd3a148d
jfrisina <jaki.frisina@gmail.com>
2024-05-07 23:37:27 -0700

    work on last problem

--- script.js
+++ script.js
@@ -78,28 +78,30 @@ for (let num = 1; num >= 1 && num <= 100; num ++) {
 
 // Use the example string provided above to test your program. Once you are confident it is working correctly, try the following string to see if your program works properly with other data:
 // Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
-let cell1;
-let cell2;
-let cell3;
-let cell4;
-let row = cell1 + ", " + cell2 + ", " + cell3 + "," + cell4;
-console.log(cell1, cell2, cell3, cell4);
 
-// pseudo code time!
-// copy all characters in a string into a cell variable. 
-// when you hit the comma, end the cell and move to the next
-// keep repeating until you hit the end of the row (aka once you get to the new line escape characters)
-// move to the next row and repeat the process - re-iterate through the characters until you hit a comma or a line ending
-
-string = "string1,string2,string3,string4\n"
+// - Loop through the characters of a given CSV string.
+// - Store each “cell” of data in a variable.
+// - When you encounter a comma, move to the next cell.
+// - When you encounter the “\r\n” sequence, move to the next “row.”
+// - Log each row of data.
+// -- You do not need to format the data, the following works well.
+// -- console.log(cell1, cell2, cell3, cell4);
 
 
-for (const c of string) {
-    if (c = ",") {
-        //then store into a variable... do I increment as cell1++??
-    } else if ((c = "/n") | (c = "/r")) {
-        //move to next line or start over as new cell1
-    } else {
-        c++
+// VARIABLES ----------------------------------------
+let string = `cell1, cell2, cell3, cell4`;
+let cell1 = ``;
+let cell2 = ``;
+let cell3 = ``;
+let cell4 = ``;
+let row = cell1 + " " + cell2 + " " + cell3 + " " + cell4; 
+
+// CODE -------------------------------------------
+for (let i = 0; i < string.length; i++) {
+    if (string[i] === " ") {
+        continue;
+    } else { 
+        cell1 += string[i]    
     }
 }
+console.log(cell1);
