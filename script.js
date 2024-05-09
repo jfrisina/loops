1203abe0f59734c9c3ada656bb3373d4f03e50a8
jfrisina <jaki.frisina@gmail.com>
2024-05-09 09:49:56 -0700

    finish part 3

--- script.js
+++ script.js
@@ -84,24 +84,63 @@ for (let num = 1; num >= 1 && num <= 100; num ++) {
 // - When you encounter a comma, move to the next cell.
 // - When you encounter the “\r\n” sequence, move to the next “row.”
 // - Log each row of data.
-// -- You do not need to format the data, the following works well.
-// -- console.log(cell1, cell2, cell3, cell4);
+    // -- You do not need to format the data, the following works well.
+    // -- console.log(cell1, cell2, cell3, cell4);
 
 
 // VARIABLES ----------------------------------------
-let string = `cell1, cell2, cell3, cell4`;
+// let string = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`
+let string = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 let cell1 = ``;
 let cell2 = ``;
 let cell3 = ``;
 let cell4 = ``;
-let row = cell1 + " " + cell2 + " " + cell3 + " " + cell4; 
+let placeholder = ``;
+let counter = 0;
 
 // CODE -------------------------------------------
-for (let i = 0; i < string.length; i++) {
-    if (string[i] === " ") {
-        continue;
-    } else { 
-        cell1 += string[i]    
+for (let char of string) {
+    switch (char) {
+        // if comma, then do this
+        case `,`:
+            counter++;
+            if (!cell1) {
+                cell1 = placeholder;
+                placeholder = ``;
+            } else if (!cell2) {
+                cell2 = placeholder;
+                placeholder = ``
+            } else {
+                cell3 = placeholder;
+                placeholder = ``;
+            }
+            break;
+        // if new line, then do this
+            case`\n`:
+                counter++;
+                cell4 = placeholder;
+                placeholder = ``; // setting placeholder back to empty
+                console.log(cell1, cell2, cell3, cell4);
+                cell1 = ``;
+                cell2 = ``;
+                cell3 = ``; 
+                cell4 = ``; // setting all cells to empty again
+                break;
+        // if character, then do this
+        default: 
+            counter++;
+            placeholder += char;
+            if (counter === string.length) {
+                cell4 = placeholder;
+                placeholder = ``; // setting placeholder back to empty
+                console.log(cell1, cell2, cell3, cell4);
+                cell1 = ``;
+                cell2 = ``;
+                cell3 = ``; 
+                cell4 = ``; // setting all cells to empty again
+                counter = 0;
+            }
+            break;
+        }
     }
-}
-console.log(cell1);
+
